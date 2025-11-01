19327372e64dd172589c1193faecfc03c6de097d
Alberto Gerardo Andres <99352303+KickedDust@users.noreply.github.com>
2025-11-01 11:32:13 -0600

    Simplificar escena con jugador esférico y plataformas básicas

--- Starter-Kit-3D-Platformer-main/scripts/main.gd
+++ Starter-Kit-3D-Platformer-main/scripts/main.gd
@@ -1,11 +1,17 @@
 extends Node3D
 
+var elapsed_time := 0.0
+
+@onready var hud: Control = $HUD
+@onready var sun: DirectionalLight3D = $Sun
+@onready var environment: WorldEnvironment = $Environment
 
 func _ready() -> void:
-	if RenderingServer.get_current_rendering_method() == "gl_compatibility":
-		# Reduce background and sun brightness when using the Compatibility renderer;
-		# this tries to roughly match the appearance of Forward+.
-		# This compensates for the different color space and light rendering for lights with shadows enabled.
-		$Sun.light_energy = 0.24
-		$Sun.shadow_opacity = 0.85
-		$Environment.environment.background_energy_multiplier = 0.25
+        if RenderingServer.get_current_rendering_method() == "gl_compatibility":
+                sun.light_energy = 0.24
+                sun.shadow_opacity = 0.85
+                environment.environment.background_energy_multiplier = 0.25
+
+func _process(delta: float) -> void:
+        elapsed_time += delta
+        hud.set_time(elapsed_time)
--- Starter-Kit-3D-Platformer-main/scripts/player.gd
+++ Starter-Kit-3D-Platformer-main/scripts/player.gd
@@ -2,162 +2,45 @@ extends CharacterBody3D
 
 signal coin_collected
 
-@export_subgroup("Components")
 @export var view: Node3D
+@export var movement_speed := 7.0
+@export var jump_strength := 7.5
 
-@export_subgroup("Properties")
-@export var movement_speed = 250
-@export var jump_strength = 7
+var coins := 0
+var gravity_strength := ProjectSettings.get_setting("physics/3d/default_gravity")
 
-var movement_velocity: Vector3
-var rotation_direction: float
-var gravity = 0
+func _physics_process(delta: float) -> void:
+        var input_direction := Vector3.ZERO
+        input_direction.x = Input.get_axis("move_left", "move_right")
+        input_direction.z = Input.get_axis("move_forward", "move_back")
 
-var previously_floored = false
+        if view:
+                input_direction = input_direction.rotated(Vector3.UP, view.rotation.y)
 
-var jump_single = true
-var jump_double = true
+        if input_direction.length() > 1.0:
+                input_direction = input_direction.normalized()
 
-var coins = 0
+        var horizontal_velocity := input_direction * movement_speed
+        velocity.x = horizontal_velocity.x
+        velocity.z = horizontal_velocity.z
 
-@onready var particles_trail = $ParticlesTrail
-@onready var sound_footsteps = $SoundFootsteps
-@onready var model = $Character
-@onready var animation = $Character/AnimationPlayer
+        if not is_on_floor():
+                velocity.y -= gravity_strength * delta
+        elif velocity.y < 0.0:
+                velocity.y = 0.0
 
-# Functions
+        if is_on_floor() and Input.is_action_just_pressed("jump"):
+                velocity.y = jump_strength
 
-func _physics_process(delta):
+        move_and_slide()
 
-	# Handle functions
+        if horizontal_velocity.length() > 0.05:
+                var target_angle := atan2(horizontal_velocity.x, horizontal_velocity.z)
+                rotation.y = lerp_angle(rotation.y, target_angle, delta * 10.0)
 
-	handle_controls(delta)
-	handle_gravity(delta)
+        if global_position.y < -20.0:
+                get_tree().reload_current_scene()
 
-	handle_effects(delta)
-
-	# Movement
-
-	var applied_velocity: Vector3
-
-	applied_velocity = velocity.lerp(movement_velocity, delta * 10)
-	applied_velocity.y = -gravity
-
-	velocity = applied_velocity
-	move_and_slide()
-
-	# Rotation
-
-	if Vector2(velocity.z, velocity.x).length() > 0:
-		rotation_direction = Vector2(velocity.z, velocity.x).angle()
-
-	rotation.y = lerp_angle(rotation.y, rotation_direction, delta * 10)
-
-	# Falling/respawning
-
-	if position.y < -10:
-		get_tree().reload_current_scene()
-
-	# Animation for scale (jumping and landing)
-
-	model.scale = model.scale.lerp(Vector3(1, 1, 1), delta * 10)
-
-	# Animation when landing
-
-	if is_on_floor() and gravity > 2 and !previously_floored:
-		model.scale = Vector3(1.25, 0.75, 1.25)
-		Audio.play("res://sounds/land.ogg")
-
-	previously_floored = is_on_floor()
-
-# Handle animation(s)
-
-func handle_effects(delta):
-
-	particles_trail.emitting = false
-	sound_footsteps.stream_paused = true
-
-	if is_on_floor():
-		var horizontal_velocity = Vector2(velocity.x, velocity.z)
-		var speed_factor = horizontal_velocity.length() / movement_speed / delta
-		if speed_factor > 0.05:
-			if animation.current_animation != "walk":
-				animation.play("walk", 0.1)
-
-			if speed_factor > 0.3:
-				sound_footsteps.stream_paused = false
-				sound_footsteps.pitch_scale = speed_factor
-
-			if speed_factor > 0.75:
-				particles_trail.emitting = true
-
-		elif animation.current_animation != "idle":
-			animation.play("idle", 0.1)
-			
-		if animation.current_animation == "walk":
-			animation.speed_scale = speed_factor
-		else:
-			animation.speed_scale = 1.0
-			
-	elif animation.current_animation != "jump":
-		animation.play("jump", 0.1)
-
-# Handle movement input
-
-func handle_controls(delta):
-
-	# Movement
-
-	var input := Vector3.ZERO
-
-	input.x = Input.get_axis("move_left", "move_right")
-	input.z = Input.get_axis("move_forward", "move_back")
-
-	input = input.rotated(Vector3.UP, view.rotation.y)
-
-	if input.length() > 1:
-		input = input.normalized()
-
-	movement_velocity = input * movement_speed * delta
-
-	# Jumping
-
-	if Input.is_action_just_pressed("jump"):
-
-		if jump_single or jump_double:
-			jump()
-
-# Handle gravity
-
-func handle_gravity(delta):
-
-	gravity += 25 * delta
-
-	if gravity > 0 and is_on_floor():
-
-		jump_single = true
-		gravity = 0
-
-# Jumping
-
-func jump():
-
-	Audio.play("res://sounds/jump.ogg")
-
-	gravity = -jump_strength
-
-	model.scale = Vector3(0.5, 1.5, 0.5)
-
-	if jump_single:
-		jump_single = false;
-		jump_double = true;
-	else:
-		jump_double = false;
-
-# Collecting coins
-
-func collect_coin():
-
-	coins += 1
-
-	coin_collected.emit(coins)
+func collect_coin() -> void:
+        coins += 1
+        coin_collected.emit(coins)
